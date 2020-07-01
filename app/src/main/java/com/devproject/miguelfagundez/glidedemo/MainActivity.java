da2ce20fae8e8e10789634173bc037555641bbff
Miguel <fagundezdev@gmail.com>
2020-06-30 22:07:10 -0400

    Image loaded with glide

--- app/src/main/java/com/devproject/miguelfagundez/glidedemo/MainActivity.java
+++ app/src/main/java/com/devproject/miguelfagundez/glidedemo/MainActivity.java
@@ -3,12 +3,42 @@ package com.devproject.miguelfagundez.glidedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Members
+    private ImageView image;
+
+    // Url
+    private String urlSquare = "https://picsum.photos/2000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupMembers();
+        setupGlidePlaceHolder();
+    }
+
+    private void setupMembers() {
+        image = findViewById(R.id.ivImage);
+    }
+
+    private void setupGlidePlaceHolder() {
+
+        // Square Image
+        Glide.with(MainActivity.this)
+                .load(urlSquare)
+                .placeholder(R.drawable.progress_bar)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(image);
     }
 }
